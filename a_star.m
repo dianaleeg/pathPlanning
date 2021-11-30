92aa15c89943b076638445cedaa227e879f2f250
dianaleeg <dianalee.g.842@gmail.com>
2021-11-29 22:16:01 -0700

    added a star. grid sizing needs correction

--- a_star.m
+++ a_star.m
@@ -1,36 +1,19 @@
-function [time_traveled, dist_traveled, map] = a_star(map_img, start_pos, end_pos)
+close all
+clc
+clear all
 
-    cost_from_start_to_next_node = 0;
-    est_cost_from_next_node_to_last_node= 0;
-    calc_cost = cost_from_start_to_next_node + est_cost_from_next_node_to_last_node;
+start = [40, 45];
+goal = [2, 2];
+a_starplanner("city_map.png", start, goal)
+
+function a_starplanner(map_img, start_pos, end_pos)
     
     %map_img = imread('city_map.png');
     grid = loadMap('city_map.png', 50);
-    start_pos = [1,1];
-    end_pos = [10,1];
-    
-    %start timer after variables are set and map is loaded
-    start_time = clock;
+    planner = plannerAStarGrid(grid);
 
-    opened = zeros(50,50); % needs to be the list of all nodes to visit
-    closed = zeros(50,50); % will populate as nodes are visited
+    plan(planner,start_pos, end_pos);
     
-    opened(1) = start_pos(1), start_pos(2)
-    opened
-    
-    %start timer once variables are set up and map loaded
-    start_time = clock; 
-    while size(opened) > 0
-        
-        %keep running algo
-        %next_node = min(opened[]) % open node with least cost
-        
-    end
-    
-    %end timer
-    end_time = clock;
-    
-    %take elapsed time
-    time_traveled = end_time - start_time
+    show(planner)
 
 end
