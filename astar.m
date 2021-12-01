ce97c3fc1d6b045b15f673f1fc9cbdae4873fe11
chanochang <70128746+chanochang@users.noreply.github.com>
2021-12-01 00:20:18 -0800

    Add files via upload
    A-star path planner

---
+++ astar.m
@@ -0,0 +1,37 @@
+function [time_traveled, dist_traveled, pthObj, solnInfo] = a_star(map, start_pos, end_pos)
+
+    %statespace
+    ss = stateSpaceSE2;
+
+    %statespace validaror
+    sv = validatorOccupancyMap(ss);
+
+    %load map
+    sv.Map = map;
+
+    %some validation distance threshold
+    sv.ValidationDistance = 0.01;
+
+    %Update state space bounds to be the same as map limits.
+    ss.StateBounds = [map.XWorldLimits;map.YWorldLimits; [-pi pi]];
+    
+    %Create the path planner and increase max connection distance.
+    planner = plannerAStarGrid(ss,sv);
+    planner.MaxConnectionDistance = 0.3;
+
+    %Plan a path with default settings.
+    rng(100,'twister'); % for repeatable result
+    
+    %start timer after variables are set and map is loaded
+    start_time = clock;
+    
+    %call planner
+    [pthObj,solnInfo] = plan(planner,start_pos,end_pos);
+    
+    %stop timer
+    end_time = clock;
+    
+    time_traveled = end_time - start_time;
+    dist_traveled = 0;
+
+end
