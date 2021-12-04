8106bb62c121d6c1bde24fd0f86d243ecdcfd9c1
Trevor Rizzo <tarizzo@wpi.edu>
2021-12-04 18:35:55 -0500

    returning endpoints

--- safe_flight_corridor.m
+++ safe_flight_corridor.m
@@ -36,156 +36,176 @@ hold on
 scatter(goal(1), goal(2), 'r', 'filled')
 scatter(start(1), start(2), 'g', 'filled')
 
-%% Step 1: Set Bounding box
 p1 = start;
 p2 = goal;
 
-v_max = 10; % m/s
-a_max = 9.8; % m/s^2
+SFC(grid, p1,p2)
 
-r_s = v_max^2 / (2 * a_max); % m
+function endpoints = SFC(grid, p1,p2)
+    %% Step 1: Set Bounding box
 
-theta = atan2(p2(2) - p1(2), p2(1) - p1(1));
-R_l_to_w = [[cos(theta), -sin(theta)]; ...
-     [sin(theta), cos(theta)]]; % rotation matrix of line segment frame to world frame
+    v_max = 10; % m/s
+    a_max = 9.8; % m/s^2
 
-R_l_to_w_3 = [[cos(theta), -sin(theta), 0]; ...
-     [sin(theta), cos(theta), 0];
-     [0, 0, 1]];
- 
-R_w_to_l_3 = [[cos(theta), sin(theta), 0]; ...
-     [-sin(theta), cos(theta), 0];
-     [0, 0, 1]]; % rotation matrix of world frame to line segment frame
- 
-bb_world = []; % Bounding box points in world frame
-bb_occupancy = [];
-for i = -r_s:r_s
-    for j = -r_s:distance(p1, p2) + r_s
-        bb_world = [bb_world; p1 + (R_l_to_w * [j; i])'];
-    end
-end
-scatter(bb_world(:,1), bb_world(:,2), 'b');
+    r_s = v_max^2 / (2 * a_max); % m
 
-%% Step 2: Calculate Ellipse 
-bb_occupancy = getOccupancy(grid, bb_world);
+    theta = atan2(p2(2) - p1(2), p2(1) - p1(1));
+    R_l_to_w = [[cos(theta), -sin(theta)]; ...
+         [sin(theta), cos(theta)]]; % rotation matrix of line segment frame to world frame
 
-if ~any(bb_occupancy(:) == 1)
-    % TODO no obstacles case
-end
+    R_l_to_w_3 = [[cos(theta), -sin(theta), 0]; ...
+         [sin(theta), cos(theta), 0];
+         [0, 0, 1]];
 
-% Ellipse midpoint
-midpoint = mean([p1; p2]);
-scatter(midpoint(1), midpoint(2), 'black');
+    R_w_to_l_3 = [[cos(theta), sin(theta), 0]; ...
+         [-sin(theta), cos(theta), 0];
+         [0, 0, 1]]; % rotation matrix of world frame to line segment frame
 
-% Ellipse length along line's x-axis
-len = distance(p1, p2);
-
-% Iterate over bounding box and find closest occupied point
-bb_size = size(bb_occupancy);
-o_closest_world = [-1, -1]; % Closest obstacle in world frame
-o_closest_dist = -1; % Distance to closest obstacle
-for i = 1:bb_size(1)
-    if bb_occupancy(i)
-        o_world = bb_world(i, :);
-        if isequal(o_closest_world, [-1, -1]) || dist(midpoint, o_world) < o_closest_dist
-            o_closest_world = o_world;
-            o_closest_dist = dist(midpoint, o_closest_world);
+    bb_world = []; % Bounding box points in world frame
+    for i = -r_s:r_s
+        for j = -r_s:distance(p1, p2) + r_s
+            bb_world = [bb_world; p1 + (R_l_to_w * [j; i])'];
         end
-            
+    end
+    % scatter(bb_world(:,1), bb_world(:,2), 'b');
+
+    bb_occupancy = getOccupancy(grid, bb_world);
+    bb_size = size(bb_occupancy);
+
+
+    endpoints = []; % Endpoints lines that create SFC polyhedron [x1_1, y1_1, x1_2, y1_2; ]
+    %% Step 2: Calculate Ellipse 
+    while ismember(1, bb_occupancy)
+
+        for i = 1:bb_size(1)
+            bb_point = bb_world(i, :);
+            if bb_occupancy(i) == 1
+                scatter(bb_point(1), bb_point(2), 'b');
+            end
+        end
+
+        % Ellipse midpoint
+        midpoint = mean([p1; p2]);
+        scatter(midpoint(1), midpoint(2), 'black');
+
+        % Ellipse length along line's x-axis
+        len = distance(p1, p2);
+
+        % Iterate over bounding box and find closest occupied point
+
+        o_closest_world = [-1, -1]; % Closest obstacle in world frame
+        o_closest_dist = -1; % Distance to closest obstacle
+        for i = 1:bb_size(1)
+            if bb_occupancy(i)
+                o_world = bb_world(i, :);
+                if isequal(o_closest_world, [-1, -1]) || dist(midpoint, o_world) < o_closest_dist
+                    o_closest_world = o_world;
+                    o_closest_dist = dist(midpoint, o_closest_world);
+                end
+
+            end
+        end
+        scatter(o_closest_world(1), o_closest_world(2), 'white');
+
+        % TODO fix this by rotating point to world coordinates then measuring
+
+        % Solve for ellipse height use pythagorean theorem
+        x = [0, grid.XWorldLimits(2)];
+        x1 = o_closest_world(1); % Specify your starting x
+        y1 = o_closest_world(2);  % Specify your starting y
+        T = [[1, 0, x1];
+            [0, 1, y1];
+            [0, 0, 1]];
+        T_neg = [[1, 0, -x1];
+                [0, 1, -y1];
+                [0, 0, 1]];
+        perpendicular = T * R_w_to_l_3 * T_neg * [x; y1 * ones(size(x)); ones(size(x))];
+        plot(perpendicular(1,:), perpendicular(2,:))
+
+        [intersect_x, intersect_y] = polyxpoly([p1(1), p2(1)], [p1(2), p2(2)], perpendicular(1,:), perpendicular(2,:));
+        point_to_line = dist([intersect_x, intersect_y], o_closest_world);
+        % point_to_line = abs(point_to_line_dist(p1, p2, o_closest_world));
+        o_y = midpoint(2) + point_to_line;
+        o_x = midpoint(1) + sqrt((point_to_line^2) + (o_closest_dist^2));
+        height = sqrt(abs(((o_y - midpoint(2))^2) / (1 - (((o_x - midpoint(1))^2) / ((len/2)^2)))));
+
+
+
+        % Define ellispe
+        x = linspace(midpoint(1)-(len/2), midpoint(1)+(len/2));
+        y_pos = midpoint(2) + sqrt((1 - (((x-midpoint(1)).^2) / ((len/2)^2))) * height);
+        y_neg = midpoint(2) - sqrt((1 - (((x-midpoint(1)).^2) / ((len/2)^2))) * height);
+
+        % x = linspace(-(len/2), (len/2));
+        % y = sqrt((1 - (((x-midpoint(1)).^2) / ((len/2)^2))) * height);
+        % 
+        T = [[1, 0, midpoint(1)];
+            [0, 1, midpoint(2)];
+            [0, 0, 1]];
+        T_neg = [[1, 0, -midpoint(1)];
+            [0, 1, -midpoint(2)];
+            [0, 0, 1]];
+        ellipse = T * R_l_to_w_3 * T_neg * [x; y_pos; ones(size(x))];
+        ellipse_neg = T * R_l_to_w_3 * T_neg * [x; y_neg; ones(size(x))];
+
+        plot(ellipse(1,:), ellipse(2,:))
+        plot(ellipse_neg(1,:), ellipse_neg(2,:))
+
+        % Find line tangent to ellipse at point o
+        syms x_sym
+        y = midpoint(2) + sqrt(abs(1 - (((x_sym-midpoint(1))^2) / ((len/2)^2))) * height);
+        y_dot = diff(y);
+        slope = vpa(subs(y_dot,x_sym,o_world(1)));
+
+    %     x = linspace(0, grid.XWorldLimits(2));
+        x = 0:grid.XWorldLimits(2);
+        x1 = o_closest_world(1); % Specify your starting x
+        y1 = o_closest_world(2);  % Specify your starting y
+        y = -slope*(x - x1) + y1; % TODO choose positive or negative slope
+        T = [[1, 0, x1];
+            [0, 1, y1];
+            [0, 0, 1]];
+        T_neg = [[1, 0, -x1];
+            [0, 1, -y1];
+            [0, 0, 1]];
+        tangent = T * R_l_to_w_3 * T_neg * [x; y; ones(size(x))];
+        plot(tangent(1,:),tangent(2,:), 'b'); % plot the graph, and store line reference in a variable.
+
+        x_bounds = [min(bb_world(:, 1)), max(bb_world(:, 1))];
+        min_idx = find(tangent(1,:) == round(x_bounds(1)));
+        max_idx = find(tangent(1,:) == round(x_bounds(2)));
+        endpoints = [endpoints; tangent(1:2,min_idx)', tangent(1:2,max_idx)'];
+        % plot(x, y,'r')
+
+        % tangent plane to the ellipsoid at this point creates a half space
+        for i = 1:bb_size(1)
+            if bb_occupancy(i) == 1
+                a =1;
+            end
+            bb_point = bb_world(i, :);
+            x = bb_point(1);
+            x1 = o_closest_world(1); % Specify your starting x
+            y1 = o_closest_world(2);  % Specify your starting y
+            y = -slope*(x - x1) + y1; % TODO choose positive or negative slope
+            T = [[1, 0, x1];
+                [0, 1, y1];
+                [0, 0, 1]];
+            T_neg = [[1, 0, -x1];
+                [0, 1, -y1];
+                [0, 0, 1]];
+            tangent_point = double(T * R_l_to_w_3 * T_neg * [x; y; ones(size(x))]);
+            if round(tangent_point(2)) >= round(midpoint(2)) && round(bb_point(2)) >= round(tangent_point(2))
+                bb_occupancy(i) = 0;
+            elseif round(tangent_point(2)) <= round(midpoint(2)) && round(bb_point(2)) <= round(tangent_point(2))
+                bb_occupancy(i) = 0;
+            elseif bb_occupancy(i) == 1
+                scatter(bb_point(1), bb_point(2), 'red');
+            end
+        end
     end
 end
-scatter(o_closest_world(1), o_closest_world(2), 'white');
 
-% TODO fix this by rotating point to world coordinates then measuring
-
-% Solve for ellipse height use pythagorean theorem
-x = [0, grid.XWorldLimits(2)];
-x1 = o_closest_world(1); % Specify your starting x
-y1 = o_closest_world(2);  % Specify your starting y
-T = [[1, 0, x1];
-    [0, 1, y1];
-    [0, 0, 1]];
-T_neg = [[1, 0, -x1];
-        [0, 1, -y1];
-        [0, 0, 1]];
-perpendicular = T * R_w_to_l_3 * T_neg * [x; y1 * ones(size(x)); ones(size(x))];
-plot(perpendicular(1,:), perpendicular(2,:))
-
-[intersect_x, intersect_y] = polyxpoly([p1(1), p2(1)], [p1(2), p2(2)], perpendicular(1,:), perpendicular(2,:));
-point_to_line = dist([intersect_x, intersect_y], o_closest_world);
-% point_to_line = abs(point_to_line_dist(p1, p2, o_closest_world));
-o_y = midpoint(2) + point_to_line;
-o_x = midpoint(1) + sqrt((point_to_line^2) + (o_closest_dist^2));
-height = sqrt(abs(((o_y - midpoint(2))^2) / (1 - (((o_x - midpoint(1))^2) / ((len/2)^2)))));
-
-
-
-% Define ellispe
-x = linspace(midpoint(1)-(len/2), midpoint(1)+(len/2));
-y_pos = midpoint(2) + sqrt((1 - (((x-midpoint(1)).^2) / ((len/2)^2))) * height);
-y_neg = midpoint(2) - sqrt((1 - (((x-midpoint(1)).^2) / ((len/2)^2))) * height);
-
-% x = linspace(-(len/2), (len/2));
-% y = sqrt((1 - (((x-midpoint(1)).^2) / ((len/2)^2))) * height);
-% 
-T = [[1, 0, midpoint(1)];
-    [0, 1, midpoint(2)];
-    [0, 0, 1]];
-T_neg = [[1, 0, -midpoint(1)];
-    [0, 1, -midpoint(2)];
-    [0, 0, 1]];
-ellipse = T * R_l_to_w_3 * T_neg * [x; y_pos; ones(size(x))];
-ellipse_neg = T * R_l_to_w_3 * T_neg * [x; y_neg; ones(size(x))];
-
-plot(ellipse(1,:), ellipse(2,:))
-plot(ellipse_neg(1,:), ellipse_neg(2,:))
-
-% Find line tangent to ellipse at point o
-syms x_sym
-y = midpoint(2) + sqrt(abs(1 - (((x_sym-midpoint(1))^2) / ((len/2)^2))) * height);
-y_dot = diff(y);
-slope = vpa(subs(y_dot,x_sym,o_world(1)));
-
-x = linspace(0, grid.XWorldLimits(2));
-x1 = o_closest_world(1); % Specify your starting x
-y1 = o_closest_world(2);  % Specify your starting y
-y = -slope*(x - x1) + y1; % TODO choose positive or negative slope
-T = [[1, 0, x1];
-    [0, 1, y1];
-    [0, 0, 1]];
-T_neg = [[1, 0, -x1];
-    [0, 1, -y1];
-    [0, 0, 1]];
-tangent = T * R_l_to_w_3 * T_neg * [x; y; ones(size(x))];
-plot(tangent(1,:),tangent(2,:), 'b'); % plot the graph, and store line reference in a variable.
-% plot(x, y,'r')
-
-% tangent plane to the ellipsoid at this point creates a half space
-for i = 1:bb_size(1)
-    bb_point = bb_world(i, :);
-    x = bb_point(1);
-    x1 = o_closest_world(1); % Specify your starting x
-    y1 = o_closest_world(2);  % Specify your starting y
-    y = -slope*(x - x1) + y1; % TODO choose positive or negative slope
-    T = [[1, 0, x1];
-        [0, 1, y1];
-        [0, 0, 1]];
-    T_neg = [[1, 0, -x1];
-        [0, 1, -y1];
-        [0, 0, 1]];
-    tangent_point = double(T * R_l_to_w_3 * T_neg * [x; y; ones(size(x))]);
-    if tangent_point(2) >= midpoint(2) && bb_point(2) >= tangent_point(2)
-        bb_occupancy(i) = 0;
-    elseif tangent_point(2) < midpoint(2) && bb_point(2) <= tangent_point(2)
-        bb_occupancy(i) = 0;
-    else
-        scatter(bb_point(1), bb_point(2), 'red');
-    end
-end
-
-
-
-% Helper Functions
+%% Helper Functions
 function d = dist(p1, p2)
     d = norm(p1 - p2);
 end
