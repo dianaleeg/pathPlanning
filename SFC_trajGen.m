f14def4e7c48b6be346816d56b418e8e8ec197eb
chris <poolec4@gmail.com>
2021-12-04 15:16:49 -0500

    removed plotting from traj gen

--- SFC_trajGen.m
+++ SFC_trajGen.m
@@ -1,26 +1,25 @@
 function [min_path, min_path_length] = SFC_trajGen(start, goal, poly_list)
-%SFC_TRAJGEN Summary of this function goes here
-%   Detailed explanation goes here
+%SFC_TRAJGEN Generates the trajectory for SFC path planning
 
-% load start, end points
-
-% load polygons
-
-figure
-hold on
-plot(start(1),start(2),'r.')
-plot(goal(1),goal(2),'r.')
+% figure
+% hold on
+% grid on
+% title('SFC Trajectory Generation Example')
+% xlabel('x')
+% ylabel('y')
+% plot(start(1),start(2),'r.')
+% plot(goal(1),goal(2),'r.')
 
 for i = 1:size(poly_list,2)
     polys{i} = polyshape(poly_list{i}(:,1)',poly_list{i}(:,2)');
-    plot(polys{i});
+    %plot(polys{i});
     if(i>1)
         intersections{i-1} = intersect(polys{i},polys{i-1});
-        plot(intersections{i-1},'FaceColor','green','FaceAlpha',1)
+        %plot(intersections{i-1},'FaceColor','green','FaceAlpha',1)
         
         intersect_grids{i-1} = gridPoly(intersections{i-1},2);
         for j = 1:length(intersect_grids{i-1})
-            plot(intersect_grids{i-1}(j,1),intersect_grids{i-1}(j,2),'b.');
+            %plot(intersect_grids{i-1}(j,1),intersect_grids{i-1}(j,2),'b.');
         end
     end
 end
@@ -46,8 +45,6 @@ while incrementing == true
        else
            index(incrementing_set) = index(incrementing_set) + 1;
        end
-        plot(nodes{i}(node_count,1),nodes{i}(node_count,2),'r.')
-        drawnow
     end
     node_count = node_count + 1;
 end
@@ -65,8 +62,8 @@ for i = 1:length(nodes{1})
 
     [Ps] = MinimumSnapGenerator(T,Pos);
     path = Ps(:,1:end-1)';
-    plot(path(:,1),path(:,2),'r')
-    drawnow
+    %plot(path(:,1),path(:,2),'r','LineWidth',0.25)
+    %drawnow
         
     len = pathLength(path);
     if len < min_path_length
@@ -75,7 +72,7 @@ for i = 1:length(nodes{1})
     end
 end
 
-plot(min_path(:,1),min_path(:,2),'b','LineWidth',2)
+%plot(min_path(:,1),min_path(:,2),'b','LineWidth',3)
 
 end
 
