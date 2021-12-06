eba55c6bbab820996d2895920eca57e25e318ed6
Chris Poole <poolec4@gmail.com>
2021-12-05 20:33:44 -0500

    filter edge cases for small intersection areas

--- SFC_trajGen.m
+++ SFC_trajGen.m
@@ -1,25 +1,42 @@
-function [min_path, min_path_length] = SFC_trajGen(start, goal, poly_list)
+function [min_path, min_path_length] = SFC_trajGen(start, goal, polys)
 %SFC_TRAJGEN Generates the trajectory for SFC path planning
 
-% figure
-% hold on
-% grid on
-% title('SFC Trajectory Generation Example')
-% xlabel('x')
-% ylabel('y')
-% plot(start(1),start(2),'r.')
-% plot(goal(1),goal(2),'r.')
+figure
+hold on
+grid on
+title('SFC Trajectory Generation Example')
+xlabel('x')
+ylabel('y')
+plot(start(1),start(2),'r.')
+plot(goal(1),goal(2),'r.')
 
-for i = 1:size(poly_list,2)
-    polys{i} = polyshape(poly_list{i}(:,1)',poly_list{i}(:,2)');
-    %plot(polys{i});
-    if(i>1)
-        intersections{i-1} = intersect(polys{i},polys{i-1});
-        %plot(intersections{i-1},'FaceColor','green','FaceAlpha',1)
-        
-        intersect_grids{i-1} = gridPoly(intersections{i-1},2);
-        for j = 1:length(intersect_grids{i-1})
-            %plot(intersect_grids{i-1}(j,1),intersect_grids{i-1}(j,2),'b.');
+for i = 1:size(polys,2)
+    plot(polys{i});
+end
+
+intersections = [];
+n = size(polys,2);
+indices = nchoosek(1:n,2);
+num_intersection = 1;
+
+for i = 1:size(indices,1)
+    if ~isempty(polys{indices(i,1)}) && ~isempty(polys{indices(i,2)})
+        temp_poly = intersect(polys{indices(i,1)},polys{indices(i,2)});
+        if (size(temp_poly.Vertices,1) ~= 0) && (size(temp_poly.Vertices,2) ~= 0)
+            intersections{num_intersection} = temp_poly;
+            plot(intersections{num_intersection},'FaceColor','green','FaceAlpha',1)
+            intersect_grids{num_intersection} = gridPoly(intersections{num_intersection},2);
+            
+            if (size(intersect_grids{num_intersection},1) == 0 || size(intersect_grids{num_intersection},2) == 0)
+                [x_center, y_center] = centroid(intersections{num_intersection}); 
+                intersect_grids{num_intersection} = [x_center, y_center; x_center, y_center];
+            end
+            
+            for j = 1:size(intersect_grids{num_intersection},1)
+                plot(intersect_grids{num_intersection}(j,1),intersect_grids{num_intersection}(j,2),'b.');
+            end
+
+            num_intersection = num_intersection + 1;
         end
     end
 end
@@ -31,13 +48,15 @@ node_count = 1;
 
 while incrementing == true 
     for i = 1:size(intersections,2)
-       nodes{i}(node_count,:) = intersect_grids{i}(index(i),:);
-       if (index(incrementing_set) == length(intersect_grids{incrementing_set}))
+       %if (index(i) < size(intersect_grids{i}))
+        nodes{i}(node_count,:) = intersect_grids{i}(index(i),:);
+       %end
+       if (index(incrementing_set) == size(intersect_grids{incrementing_set},1))
            if (incrementing_set >= size(intersections,2))
                incrementing = false;
            elseif (incrementing_set + 1 <= size(intersections,2))
                index(incrementing_set + 1) = index(incrementing_set + 1) + 1;
-               if (index(incrementing_set + 1) == length(intersect_grids{incrementing_set + 1}))
+               if (index(incrementing_set + 1) == size(intersect_grids{incrementing_set + 1},1))
                    incrementing_set = incrementing_set + 1;
                end
            index(1:incrementing_set) = 1;
@@ -51,7 +70,7 @@ end
 
 min_path_length = inf;
 
-for i = 1:length(nodes{1})
+for i = 1:size(nodes{1},1)
     Pos = start;
     for j = 1:size(intersections,2)
         Pos(j+1,:) = nodes{j}(i,:); 
@@ -62,8 +81,8 @@ for i = 1:length(nodes{1})
 
     [Ps] = MinimumSnapGenerator(T,Pos);
     path = Ps(:,1:end-1)';
-    %plot(path(:,1),path(:,2),'r','LineWidth',0.25)
-    %drawnow
+    plot(path(:,1),path(:,2),'r','LineWidth',0.25)
+    drawnow
         
     len = pathLength(path);
     if len < min_path_length
@@ -72,7 +91,7 @@ for i = 1:length(nodes{1})
     end
 end
 
-%plot(min_path(:,1),min_path(:,2),'b','LineWidth',3)
+plot(min_path(:,1),min_path(:,2),'b','LineWidth',3)
 
 end
 
